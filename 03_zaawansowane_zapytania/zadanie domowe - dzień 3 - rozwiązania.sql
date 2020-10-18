210f6b4a3abc5a65f0c6c31c58b1fceb1cdc26f3
MirekPz <mirek@info-tur.pl>
2020-10-19 01:34:53 +0200

    zadanie domowe z 3 dnia  (niedokończone)

---
+++ 03_zaawansowane_zapytania/zadanie domowe - dzień 3 - rozwiązania.sql
@@ -0,0 +1,30 @@
+-- 1.   Dla każdego pracownika wygeneruj kod składający się z dwóch pierwszych liter jego etatu i jego numeru id. 
+--      Wyświetl imię i nazwisko oraz kod pracownika.
+
+SELECT FIRSTNAME, LASTNAME, concat(substr(POSITION, 1, 2),ID_USER) AS Kod FROM users;
+
+
+
+-- 2.   Z nazwisk pracowników pozbądź się liter K i L - zamień je na X.
+
+SELECT lastname, replace(replace(lastname, 'K', 'X'), 'L', 'X') AS modified_lastname FROM users;
+
+
+
+-- 3.   Wyświetl tylko tych pracowników, którzy mają literę L w drugiej połowie nazwiska. 
+--      Uwaga: przyjęte założenie - jeżeli nazwisko ma nieprzystą liczbę liter, to środkową zaliczmy do drugiej połowy. 
+
+SELECT 
+	lastname, 
+    substr(lastname,floor(length(lastname)/2) + 1) AS 'Druga połowa nazwiska'  
+FROM 
+	USERS 
+WHERE 
+	instr(substr(lastname,floor(length(lastname)/2) + 1),'L') > 0; 
+
+
+
+-- 4.   Nadaj podwyżki pracownikom - powiększ ich pensje podstawową o 15% i zaokrąglij do liczb całkowitych. 
+--      Wyświetl: 
+--                imię, nazwisko, stara podstawa, nowa podstawa wynagrodzenia.
+
