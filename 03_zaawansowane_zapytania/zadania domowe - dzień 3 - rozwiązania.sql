a02b3cea6a89bccb83b585225b523477bfe0ddf8
MirekPz <mirek@info-tur.pl>
2020-10-19 03:03:14 +0200

    zadanie domowe z 3 dnia

--- 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
+++ 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
@@ -50,4 +50,17 @@ FROM
 	
 
 
--- 6. 
+-- 6.   Wyświetl nazwisko oraz datę zatrudnienia pracownika zespołu 20 w zmienionym formacie: 
+--      zamiast rok - miesiąc - dzień wyświetl: miesiąc słownie | dzień / rok
+
+SELECT 
+	lastname AS Nazwisko,
+    date_format(hired_at, '%M | %d / %Y') AS 'Data zatrudnienia'
+FROM 
+	users
+WHERE 
+	ID_TEAM = 20;
+
+
+
+-- 7.	
