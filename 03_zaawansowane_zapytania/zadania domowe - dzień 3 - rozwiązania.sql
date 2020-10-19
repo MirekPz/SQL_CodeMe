4e753df0c365eec394acffc1efabf3a6fd5edd6b
MirekPz <mirek@info-tur.pl>
2020-10-19 03:35:49 +0200

    zadanie domowe z 3 dnia

--- 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
+++ 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
@@ -70,4 +70,20 @@ WHERE
  
 
 
--- 8.  
+-- 8.   Korzystając z wyrażenia CASE zmień adresy zespołów tak by wyświetlać stan zamiast miasta.
+
+SELECT 
+	id_team, 
+    name, 
+    CASE
+		WHEN address = 'NEW YORK CITY' THEN 'New York'
+		WHEN address = 'HOUSTON' THEN 'Texas'
+		WHEN address = 'SAN DIEGO' THEN 'California'
+		WHEN address = 'LOS ANGELES' THEN 'California'
+	END AS 'Stan'
+FROM 
+	teams;
+
+
+
+-- 9. 
