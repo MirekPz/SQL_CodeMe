50f8afcf14e34e70bbbe761f1234d7f2c14e6b50
MirekPz <mirek@info-tur.pl>
2020-10-19 03:09:25 +0200

    zadanie domowe z 3 dnia

---
+++ 03_zaawansowane_zapytania/zad_dom-07.sql
@@ -0,0 +1,5 @@
+-- 7.   Sprawdź, jaki mamy dziś dzień tygodnia. 
+ --     Wyświetl nazwę dnia.
+ 
+ SELECT date_format(current_date, '%W');
+ 
--- 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
+++ 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
@@ -63,4 +63,11 @@ WHERE
 
 
 
--- 7.	
+-- 7.   Sprawdź, jaki mamy dziś dzień tygodnia. 
+ --     Wyświetl nazwę dnia.
+ 
+ SELECT date_format(current_date, '%W');
+ 
+
+
+-- 8.  
