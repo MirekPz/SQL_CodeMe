3eb9da7f75f9df51d91992a59f9c5f23334d3056
MirekPz <mirek@info-tur.pl>
2020-10-19 03:48:41 +0200

    zadanie domowe z 3 dnia

--- 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
+++ 03_zaawansowane_zapytania/zadania domowe - dzień 3 - rozwiązania.sql
@@ -86,4 +86,22 @@ FROM
 
 
 
--- 9. 
+-- 9.   Dla każdego pracownika wyświetl informację o tym, czy jego pensja jest mniejsza niż, równa lub większa niż 480
+--      |  name | salary_base |     limit     | 
+--      | smith |     450     | less than 480 | 
+
+SELECT 
+	firstname, 
+	lastname, 
+    salary_base, 
+    CASE
+		WHEN salary_base < 480 THEN 'less than 480'
+        WHEN salary_base = 480 THEN 'equals 480'
+        ELSE 'more than 480'
+    END AS 'limit' 
+FROM 
+	users;
+
+
+
+-- 10. 
