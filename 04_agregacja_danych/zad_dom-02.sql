a5d6b6af1234c7e5501096e9065dc92515256762
MirekPz <mirek@info-tur.pl>
2020-10-20 03:15:48 +0200

    Dzień 4

---
+++ 04_agregacja_danych/zad_dom-02.sql
@@ -0,0 +1,20 @@
+-- 2.   Dla każdego pracownika wyświetl informację o tym, czy jego pensja jest mniejsza niż, równa lub większa niż 480
+--    ___________________________________________
+--     | name  | salary_base |     limit        | 
+--    ------------------------------------------- 
+--     | metei |    450      |   less than 480  |
+--     | aimet |    480      |   equal 480      | 
+--     | maya  |    500      |   more than 480  |
+--    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+
+SELECT 
+	firstname, 
+	lastname, 
+    salary_base, 
+    CASE
+		WHEN salary_base < 480 THEN 'less than 480'
+        WHEN salary_base = 480 THEN 'equals 480'
+        ELSE 'more than 480'
+    END AS 'limit' 
+FROM 
+	users;
